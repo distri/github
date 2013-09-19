78d1c6f8455d35c12bbd8c81820f3334ad116109
Daniel X Moore <yahivin@gmail.com>
2013-09-19 12:05:38 -0700

    Built reduce-publish-commits in browser in strd6.github.io/editor

---
+++ reduce-publish-commits.js
@@ -0,0 +1,78 @@
+(function() {
+  var Github, Repository;
+
+  Repository = require("./repository");
+
+  Github = function(tokenPromise) {
+    var api, lastRequest, token;
+    token = null;
+    if (tokenPromise != null) {
+      tokenPromise.then(function(tokenValue) {
+        return token = tokenValue;
+      });
+    }
+    lastRequest = Observable();
+    api = function(path, options) {
+      var url;
+      if (options == null) {
+        options = {};
+      }
+      if (path.match(/^http/)) {
+        url = path;
+      } else {
+        url = "https://api.github.com/" + path;
+      }
+      options.headers || (options.headers = {});
+      if (token) {
+        options.headers["Authorization"] = "token " + token;
+      }
+      options = Object.extend({
+        url: url,
+        type: "GET",
+        dataType: 'json'
+      }, options);
+      return $.ajax(options).done(function(data, status, request) {
+        return lastRequest(request);
+      }).fail(lastRequest);
+    };
+    return {
+      api: api,
+      lastRequest: lastRequest,
+      token: function(newValue) {
+        if (arguments.length > 0) {
+          return token = newValue;
+        } else {
+          return token;
+        }
+      },
+      Repository: function(data) {
+        if (data == null) {
+          data = {};
+        }
+        Object.extend(data, {
+          requester: api
+        });
+        return Repository(data);
+      },
+      repository: function(fullName) {
+        return api("repos/" + fullName).then(function(data) {
+          Object.extend(data, {
+            requester: api
+          });
+          return Repository(data);
+        });
+      },
+      authorizaionUrl: Github.authorizationUrl
+    };
+  };
+
+  Github.authorizationUrl = function(clientId, scope) {
+    if (scope == null) {
+      scope = "user:email";
+    }
+    return "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=" + scope;
+  };
+
+  module.exports = Github;
+
+}).call(this);
