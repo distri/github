569ecbb728a8b4245f83149f7fd5c5b7a06ea8e8
Daniel X Moore <yahivin@gmail.com>
2014-04-09 17:29:44 -0700

    :dragon::palm_tree: Updated in browser at strd6.github.io/editor

--- v0.4.3-pre.1/docs/package.js
+++ v0.4.3-pre.1/docs/package.js
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html manifest="manifest.appcache?1397089777535">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-</head>
-<body>
-<script>
-;(function(PACKAGE) {
-var oldRequire = window.Require;
-(function() {
+(function(pkg) {
+  (function() {
   var annotateSourceURL, cacheFor, circularGuard, defaultEntryPoint, fileSeparator, generateRequireFn, global, isPackage, loadModule, loadPackage, loadPath, normalizePath, rootModule, startsWith,
     __slice = [].slice;
 
@@ -172,9 +163,7 @@ var oldRequire = window.Require;
 }).call(this);
 
 //# sourceURL=main.coffee
-var require = Require.generateFor(PACKAGE);
-window.Require = oldRequire;
-require('./main')
+  window.require = Require.generateFor(pkg);
 })({
   "source": {
     "LICENSE": {
@@ -1394,6 +1383,3 @@ require('./main')
     }
   }
 });
-</script>
-</body>
-</html>
